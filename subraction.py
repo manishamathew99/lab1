437e1e7c9a58af6505500ab949e8d4a3853f03c1
manishamathew99 <manishamathew9299@gmail.com>
2024-01-28 23:36:14 -0500

    Subraction script added

---
+++ subraction.py
@@ -0,0 +1,10 @@
+# This program subract two numbers
+
+num1 = 12
+num2 = 45
+
+# Add two numbers
+sub = num1 - num2
+
+# Display the Diff
+print('The diff of {0} and {1} is {2}'.format(num1, num2, sub))
